eccac9c5cfd196cb64e068470bd43e5c1145752c
Ian B <ibansenauer@gmail.com>
2022-01-27 16:15:12 -0800

    revised input parsing to (connector, pin) tuple format

--- src/pyside2/inputparser.py
+++ src/pyside2/inputparser.py
@@ -16,16 +16,23 @@ class InputParser:
 
     #Reads the PDC fuse map CSV and returns a list of the dictionaries
     #Each element in the list is a row of the PDC file, in dict format
+    #{CONNECTOR: (component,pin), FUSE:fuse rating}
     def readPDC(self, fileName):
 
         if fileName:
+            contents_list = []
             with open(fileName, mode='r') as csv_file:
                 pdcDict = csv.DictReader(csv_file, delimiter=',')
                 print("Successfully opened pdc fuse map..")
                 name = os.path.basename(fileName)
+                for line in pdcDict:
+                    contents = {}
+                    contents["CONNECTOR"] = (line["CONNECTOR"], line["PIN"])
+                    contents["FUSE"] = line["FUSE RATING"]
+                    contents_list.append(contents)
                 if name not in self.pdcs.keys():
-                    self.pdcs[name] = list(pdcDict)
-                return list(pdcDict)
+                    self.pdcs[name] = contents_list
+                return contents_list
 
         else:
             print("invalid filename passed to readPDC...")
@@ -41,5 +48,3 @@ class InputParser:
     #get the current list of report objects
     def getReports(self):
         return self.reports
-
-    
