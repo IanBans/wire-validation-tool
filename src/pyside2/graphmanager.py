5d7271c49d14264e44d25a1beb3089eb292cbedd
Ian B <ibansenauer@gmail.com>
2022-02-15 17:17:18 -0800

    removed old find_splices, cleanup of imports and comments

--- src/pyside2/graphmanager.py
+++ src/pyside2/graphmanager.py
@@ -1,5 +1,3 @@
-from openpyxl import *
-from report import Report
 import networkx as nx
 '''
    GraphManager: Class to handle graph operations, reading, writing, traversal
@@ -10,7 +8,6 @@ import networkx as nx
     addReport(): adds the specified report object to the graph, creating edges between from
         and to (component, pin) combinations
     with attributes
-    findSplices(): finds loops in a directed graph. W.I.P.
 '''
 class GraphManager:
 
@@ -18,10 +15,10 @@ class GraphManager:
         self.g = nx.Graph()
 
 
-    #addPDC(pdc_list)
-    #pdc_list: list of dictionaries of pdc returned from InputParser.readPDC()
+    # addPDC(pdc_list)
+    # pdc_list: list of dictionaries of pdc returned from InputParser.readPDC()
     # Adds data from a fuse map list to the graph.
-    #updates fuse_rating attribute if nodes already exist
+    # updates fuse_rating attribute if nodes already exist
     def addPDC(self, pdc_list):
 
         for row in pdc_list:
@@ -30,16 +27,17 @@ class GraphManager:
             vpin = row['CONNECTOR'][1]
             vname = vconn + "|" + vpin
             vfuse = int(row["FUSE"])
-            #if vertex doesn't exist, create it
+            # if vertex doesn't exist, create it
             if vname not in self.g:
                 self.g.add_node(vname, connector=vconn, pin=vpin, fuse_rating=vfuse)
-            else: #if vertex exists, update fuse rating
+            # if vertex exists, update fuse rating
+            else:
                 self.g.nodes[vname]['fuse_rating'] = vfuse
 
         print('added  pdc to graph')
 
-    #addReport(report)
-    #report: report object to add (from InputParser.readReport())
+    # addReport(report)
+    # report: report object to add (from InputParser.readReport())
     # Adds nodes from a wire report object to the graph. Graph cannot be empty.
     # adds wires between nodes in the report, updating wire csa and wire description
     def addReport(self, report):
@@ -49,9 +47,9 @@ class GraphManager:
         for row in contents:
             f_tup = row["FROM"]
             t_tup = row["TO"]
-            desc_col = row["DESC"]
-            csa_col = row["CSA"]
-            #get vertex information
+            wire_desc = row["DESC"]
+            wire_csa = row["CSA"]
+            # get vertex information
             fconn = str(f_tup[0])
             fpin = str(f_tup[1])
             fname = fconn + "|" + fpin
@@ -59,35 +57,13 @@ class GraphManager:
             tpin = str(t_tup[1])
             tname = tconn + "|" + tpin
 
-            #create vertices that don't exist
+            # create vertices that don't exist
             if fname not in self.g:
                 self.g.add_node(fname, connector=fconn, pin=fpin, fuse_rating=-1)
             if tname not in self.g:
                 self.g.add_node(tname, connector=tconn, pin=tpin, fuse_rating=-1)
 
-            #get edge information
-            ewire = desc_col
-            ecsa = csa_col
-
-            #create wire between the two components
-            self.g.add_edge(fname, tname, wire=ewire, csa=ecsa)
+            # create wire between the two components
+            self.g.add_edge(fname, tname, wire=wire_desc, csa=wire_csa)
 
         print('added ', report.filename, ' to graph')
-
-    #findSplices(report)
-    #TODO: only works on directed graphs
-    #Work in progress
-    def findSplices(self, i=0, tracking_list=[]):
-        if(i in tracking_list):
-            print("loop detected starting at ", i)
-            return
-        tracking_list.append(i)
-        neighbors = self.g.neighbors(i, mode="out")
-
-        if len(neighbors) > 1:
-            print("splice with ", i, "to ", neighbors)
-            for x in neighbors:
-                self.findSplices(x, tracking_list)
-        elif len(neighbors) == 1:
-            print(neighbors)
-            self.findSplices(neighbors[0], tracking_list)
