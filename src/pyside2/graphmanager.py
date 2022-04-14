3b2d2c079327f57241b875d6389a7b6a24d4d3bf
Ian B <ibansenauer@gmail.com>
2022-04-14 15:46:17 -0700

    ensures that traversal will handle multi-connection loops with bfs

--- src/pyside2/graphmanager.py
+++ src/pyside2/graphmanager.py
@@ -54,7 +54,7 @@ class GraphManager:
             else:
                 self._g.nodes[vname]['fuse_rating'] = vfuse
 
-        print('added  pdc to graph')
+        print('added pdc to graph')
 
     def addReport(self, report):
         """
@@ -74,6 +74,7 @@ class GraphManager:
             fpin = str(f_tup[1])
             tconn = str(t_tup[0])
             tpin = str(t_tup[1])
+            # ignore the pin for splice entries
             if fconn[0] != 'S':
                 fname = fconn + "|" + fpin
             else:
@@ -83,7 +84,7 @@ class GraphManager:
             else:
                 tname = tconn
 
-            # create vertices that don't exist
+            # create new vertices with a negative fuse rating
             if fname not in self._g:
                 self._g.add_node(fname, connector=fconn, pin=fpin, fuse_rating=-1)
             if tname not in self._g:
@@ -92,82 +93,42 @@ class GraphManager:
             self._g.add_edge(fname, tname, wire=wire_desc, csa=wire_csa)
         print('added ', report.filename, ' to graph')
 
-    def traverse(self):
-        """
-            Traces each wire in the graph from the PDC to its endpoint.
-            Returns a list of 3-tuples, where each tuple represents a wire.
-            The first element is the name of the start vertex.
-            The second element is the name of the end vertex.
-            The third element is a dictionary containing the following keys:
-              min_csa: Float, lowest CSA of any wire segment.
-        """
-        fuse_rating = nx.get_node_attributes(self._g, "fuse_rating")
-        tuples = []
-        for node in self._g:
-            if fuse_rating[node] > 0:
-                for nbr in self._g[node]:
-                    trace = self.rtraverse(node, nbr, node, {'min_csa': math.inf})
-                    for wire in trace:
-                        tuples += [wire]
-        return tuples
-
-    def rtraverse(self, startnode, currnode, lastnode, data):
-        """
-            Recursive subroutine of traverse(). Should not be called directly.
-                startnode: The first node in the wire.
-                currnode: The node added by the calling method.
-                lastnode: The node added prior to currnode.
-                data: Dictionary of information to report.
-            Key structure is in the comment for traverse().
-            TODO: Add loop handling without loop removal.
-        """
-        output = []
-        # update data with wire from lastnode to currnode
-        if self._g[lastnode][currnode]["csa"] < data["min_csa"]:
-            data["min_csa"] = self._g[lastnode][currnode]["csa"]
-        # recursively traverse to all neighbors other than lastnode
-        for nbr in self._g[currnode]:
-            if nbr != lastnode:
-                trace = self.rtraverse(startnode, nbr, currnode, data)
-                for wire in trace:
-                    output += [wire]
-        # check if this is the end of the wire
-        if not output:
-            output = [(startnode, currnode, data["min_csa"])]
-
-        return output
-
     def reportCycle(self, pdc):
         """
             Arguments:
                 pdc: node in the graph to check for cycle
             checks for a cycle within the graph from 'pdc' source.
             if one exists, gets minimum CSA, wires, and splice points.
-            returns False if no cycle, or output tuple with cycle information.
+            returns False if no cycle, or tuple with cycle information
+            and the set of wires traversed.
             output format:
             (startComponent|startPin, endComponent|endPin, min_csa, wire1, wire2, ..., wire_n,
-            splice1, splice2, etc)
+            splice1, splice2, etc), set_of_wires
         """
+        # tracks the minimum CSA, wire names, and splices
+        # across each wire trace
         min_csa = math.inf
         wires = set()
         splice_list = []
-        modified = True
+        search = True
+        # check for a cycle, if no cycle, return False
         try:
             loop = dict(nx.find_cycle(self._g, source=pdc))
         except nx.exception.NetworkXNoCycle:
             return False
         else:
             # transform values to expected list format
+            # for consistency with bfs_successors
             for key, value in loop.items():
-                l = []
-                if type(value) is not list:
-                    l.append(value)
-                    loop[key] = l
+                nodes_list = []
+                if not isinstance(value, list):
+                    nodes_list.append(value)
+                    loop[key] = nodes_list
 
-            loop_start = list(loop)[0]
-            while modified:
+            loop_head = list(loop)[0]
+            while search:
                 for start, ends in loop.items():
-                    modified = False
+                    search = False
                     # each wire, update the minimum CSA and add to
                     # the wires list
                     for end in ends:
@@ -177,14 +138,16 @@ class GraphManager:
                             wires.add(wname)
                             if curr_csa < min_csa:
                                 min_csa = curr_csa
+                            # if a splice is detected, there is potential for more ends
+                            # a BFS is needed to record all the wires.
+                            # set 'modified' flag to update the iterator
                             if end[0] == 'S':
                                 splice_list.append(end)
                                 loop.update(nx.bfs_successors(self._g, end))
-                                modified = True
+                                search = True
 
-
-            return (loop_start, loop_start, min_csa, ', '.join(wires), ', '.join(splice_list))
-
+            return ( (loop_head, loop_head, min_csa, ', '.join(wires), ', '.join(splice_list)),
+                     wires )
 
     def spliceTraverse(self):
         '''
@@ -206,40 +169,47 @@ class GraphManager:
             path['min_csa'] = math.inf
             path['wires'] = set()
             path['splice'] = set()
+            loop_wires = set()
             loop = self.reportCycle(pdc)
+            # when there's a loop, temporarily modify the
+            # wire set to avoid tracing the same wires twice
             if loop:
-                output.append(loop)
-            else:
-                search = dict(nx.bfs_successors(self._g, source=pdc))
+                output.append(loop[0])
+                path['wires'].update(loop[1])
+                loop_wires.update(loop[1])
+            search = dict(nx.bfs_successors(self._g, source=pdc))
+            if bool(search):
+                for start, ends in search.items():
+                    # each wire, update the minimum CSA and add to
+                    # the wires list
+                    for end in ends:
+                        curr_csa = self._g[start][end]['csa']
+                        wname = self._g[start][end]['wire']
+                        if wname not in path['wires']:
+                            path['wires'].add(wname)
+                            if curr_csa < path['min_csa']:
+                                path['min_csa'] = curr_csa
+                            # when the wire is not in the list of traversed nodes,
+                            # it's an endpoint or a loop
+                            if end not in set(search.keys()):
+                                path['end'] = end
+                                if start[0] == 'S':
+                                    path['splice'].add(start)
 
-                if bool(search):
-                    for start, ends in search.items():
-                        # each wire, update the minimum CSA and add to
-                        # the wires list
-                        for end in ends:
-                            curr_csa = self._g[start][end]['csa']
-                            wname = self._g[start][end]['wire']
-                            if wname not in path['wires']:
-                                path['wires'].add(wname)
-                                if curr_csa < path['min_csa']:
-                                    path['min_csa'] = curr_csa
-                                # when the wire is not in the list of traversed nodes, it's an endpoint
-                                # or a loop
-                                if end not in set(search.keys()):
-                                    path['end'] = end
-                                    if start[0] == 'S':
-                                        path['splice'].add(start)
-                                        search = dict(nx.bfs_successors(self._g, source=start))
+                                splice_list = list(path['splice'])
 
-                                    splice_list = list(path['splice'])
-                                    output.append((path['start'], path['end'], path['min_csa'],
-                                                   ', '.join(path['wires']), ', '.join(splice_list)))
+                                # remove the loop wires from the path wire set
+                                # before adding to output
+                                wire_set = path['wires'].difference(loop_wires)
+                                output.append((path['start'], path['end'], path['min_csa'],
+                                                  ', '.join(wire_set), ', '.join(splice_list)))
 
         return sorted(output, key=lambda y: y[0])
 
     def printTraverse(self):
         '''
             Prints the result of spliceTraverse
+            to console
             Testing helper function. remove on release
         '''
         for item in self.spliceTraverse():
@@ -254,6 +224,6 @@ class GraphManager:
         pdcs.sort()
         try:
             for node in pdcs:
-                print(node, ':', dict(nx.find_cycle(self._g, node)))
+                print(node, ':', dict(nx.bfs_successors(self._g, node)))
         except nx.exception.NetworkXNoCycle:
             print('no cycles')
